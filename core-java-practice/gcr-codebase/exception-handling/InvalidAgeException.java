3ed7fad8048c519ec5995b7ed729a4ebdd6cef5d
ashish-yadavv <yash310555@gmail.com>
2026-06-22 13:10:33 +0530

    Remove core-java-practice folder

--- core-java-practice/gcr-codebase/exception-handling/InvalidAgeException.java
+++
@@ -1,5 +0,0 @@
-class InvalidAgeException extends Exception {
-    public InvalidAgeException(String message) {
-        super(message);
-    }
-}
--- core-java-practice/gcr-codebase/programming-elements/level1/KilometerToMilesConverter.java
+++
@@ -1,9 +0,0 @@
-class KilometerToMilesConverter {
-    public static void main(String[] args) {
-        double distanceInKm = 10.8;
-        double milesPerKm = 1.6;
-        double distanceInMiles = distanceInKm * milesPerKm;
-
-        System.out.println("The distance " + distanceInKm + " km in miles is " + distanceInMiles);
-    }
-}
